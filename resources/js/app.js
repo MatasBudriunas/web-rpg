527356ec8a4b5ce309f19381fa9c8c793fa7747f
MatasBudriunas <zewas1m@gmail.com>
2023-08-13 15:46:25 +0700

    extract router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,35 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
-import AuthForm from './AuthForm.vue';
-import Dashboard from './Dashboard.vue';
 import store from './store.js';
 import './axios-config';
-
-const routes = [
-    { path: '/', redirect: '/dashboard' }, // Redirect / to /dashboard
-    { path: '/auth', component: AuthForm },
-    { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
-];
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes
-});
-
-router.beforeEach((to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)) {
-        const isAuthenticated = !!localStorage.getItem('auth-token');
-        if (!isAuthenticated) {
-            next('/auth');
-        } else {
-            next();
-        }
-    } else {
-        next();
-    }
-});
+import router from './router.js';
 
 const app = createApp(App);
 app.use(router);
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,29 +1,29 @@
-// import { createRouter, createWebHistory } from 'vue-router';
-// import AuthForm from './AuthForm.vue';
-// import Dashboard from './Dashboard.vue';
-//
-// const routes = [
-//     { path: '/', redirect: '/dashboard' },
-//     { path: '/auth', component: AuthForm },
-//     { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
-// ];
-//
-// const router = createRouter({
-//     history: createWebHistory(),
-//     routes
-// });
-//
-// router.beforeEach((to, from, next) => {
-//     if (to.matched.some(record => record.meta.requiresAuth)) {
-//         const isAuthenticated = !!localStorage.getItem('auth-token');
-//         if (!isAuthenticated) {
-//             next('/auth');
-//         } else {
-//             next();
-//         }
-//     } else {
-//         next();
-//     }
-// });
-//
-// export default router;
+import { createRouter, createWebHistory } from 'vue-router';
+import AuthForm from './AuthForm.vue';
+import Dashboard from './Dashboard.vue';
+
+const routes = [
+    { path: '/', redirect: '/dashboard' },
+    { path: '/auth', component: AuthForm },
+    { path: '/dashboard', component: Dashboard, meta: { requiresAuth: true } },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        const isAuthenticated = !!localStorage.getItem('auth-token');
+        if (!isAuthenticated) {
+            next('/auth');
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
+export default router;
